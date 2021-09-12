00906087a15dbc92f78a392d314071ecdba2af9d
Your Name <you@example.com>
2021-09-12 12:35:39 +0100

    🍺微不足道

--- tool/startAdvertise.sh
+++ tool/startAdvertise.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-
-cd /home/pi/config/_build/prod/rel/gateway_config/
-./bin/gateway_config start 
-sleep 3
-./bin/gateway_config advertise on
+/home/pi/config/_build/prod/rel/gateway_config/bin/gateway_config start 
+sleep 1
+/home/pi/config/_build/prod/rel/gateway_config/bin/gateway_config ping
+sleep 1
+/home/pi/config/_build/prod/rel/gateway_config/bin/gateway_config advertise on
